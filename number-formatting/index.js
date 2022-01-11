a48e443d4b63049065e8dcb2dd8c3c1c234da1ec
CoradexID <coradex06@gmail.com>
2022-01-11 19:41:44 +0700

    Update number-formatting/index.js

--- number-formatting/index.js
+++ number-formatting/index.js
@@ -6,22 +6,20 @@
 */
 
 
-
-function intFormat(angka, prefix = null){
-	var number_string = angka.toString().replace(/[^,\d]/g, ''),
+function intFormat(int){
+	var number_string = int.toString().replace(/[^,\d]/g, ''),
 	split   		= number_string.split(','),
-	sisa     		= split[0].length % 3,
-	rupiah     		= split[0].substr(0, sisa),
-	ribuan     		= split[0].substr(sisa).match(/\d{3}/gi);
+	remain     		= split[0].length % 3,
+	rupiah     		= split[0].substr(0, remain),
+	final     		= split[0].substr(remain).match(/\d{3}/gi);
  
-	// tambahkan titik jika yang di input sudah menjadi angka ribuan
-	if(ribuan){
-		separator = sisa ? ',' : '';
-		rupiah += separator + ribuan.join(',');
+	if(final){
+		separator = remain ? ',' : '';
+		rupiah += separator + final.join(',');
 	}
  
 	rupiah = split[1] != undefined ? rupiah + ',' + split[1] : rupiah;
-	return prefix == null ? rupiah : (rupiah ? 'Rp. ' + rupiah : '');
+	return 'Rp. ' + rupiah;
 }
 
 const formatted = intFormat(1000000000);
